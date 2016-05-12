224c01ad8f5d793e3f95080be7f068cca2bb1483
Ricardo Moran <Ricardo Moran>
2016-05-12 16:31:09 -0300

    Added a simple library that wraps C# IEnumerator in a Stream interface.

---
+++ Streams/Stream.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams
+{
+    public class Stream<T>
+    {
+        private IEnumerator<T> source;
+        private int position = 0;
+        private bool atEnd = false;
+
+        public Stream(IEnumerator<T> source)
+        {
+            this.source = source;
+            atEnd = !source.MoveNext();
+        }
+
+        public Stream(IEnumerable<T> source) : this(source.GetEnumerator()) { }
+
+        public int Position { get { return position; } }
+        public bool AtEnd { get { return atEnd; } }
+
+        public T Next()
+        {
+            if (AtEnd) return default(T);
+            T result = source.Current;
+            atEnd = !source.MoveNext();
+            position++;
+            return result;
+        }
+
+        public IEnumerable<T> Next(int count)
+        {
+            List<T> result = new List<T>();
+            int i = 0;
+            while (!AtEnd && count > i++)
+            {
+                result.Add(Next());
+            }
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (AtEnd) return default(T);
+            return source.Current;
+        }
+
+        public IEnumerable<T> UpTo(Func<T, bool> condition)
+        {
+            List<T> result = new List<T>();
+            T next;
+            while (!AtEnd && !condition(next = Next()))
+            {
+                result.Add(next);
+            }
+            return result;
+        }
+        
+        public IEnumerable<T> UpTo(T element)
+        {
+            return UpTo((each) => Equals(element, each));
+        }
+
+        public IEnumerable<T> UpToEnd()
+        {
+            List<T> result = new List<T>();
+            while (!AtEnd)
+            {
+                result.Add(Next());
+            }
+            return result;
+        }
+    }
+}
---
+++ StreamsTest/StreamTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Streams;
+
+namespace StreamsTest
+{
+    [TestClass]
+    public class StreamTest
+    {
+        [TestMethod]
+        public void EmptyStreamStartsAtEnd()
+        {
+            Stream<int> stream = new Stream<int>(new int[0]);
+            Assert.IsTrue(stream.AtEnd);
+        }
+
+        [TestMethod]
+        public void EmptyStreamReturnsDefaultValue()
+        {
+            Stream<int> s1 = new Stream<int>(new int[0]);
+            Assert.AreEqual(0, s1.Next());
+            Assert.AreEqual(0, s1.Peek());
+
+            Stream<object> s2 = new Stream<object>(new object[0]);
+            Assert.IsNull(s2.Next());
+            Assert.IsNull(s2.Peek());
+        }
+
+        [TestMethod]
+        public void SendingNextToEmptyStreamAlwaysReturnsDefaultValue()
+        {
+            Stream<object> stream = new Stream<object>(new object[0]);
+            Assert.IsNull(stream.Next());
+            Assert.IsNull(stream.Next());
+            Assert.IsNull(stream.Next());
+        }
+
+        [TestMethod]
+        public void NextAdvancesTheStreamPosition()
+        {
+            Stream<int> stream = new Stream<int>(new int[3] { 1, 2, 3 });
+            Assert.AreEqual(0, stream.Position);
+            stream.Next();
+            Assert.AreEqual(1, stream.Position);
+        }
+
+        [TestMethod]
+        public void NextAdvancesTheStreamPositionUntilTheEnd()
+        {
+            Stream<int> stream = new Stream<int>(new int[3] { 1, 2, 3 });
+            stream.Next();
+            stream.Next();
+            stream.Next();
+            Assert.IsTrue(stream.AtEnd);
+            stream.Next();
+            Assert.IsTrue(stream.AtEnd);
+        }
+
+        [TestMethod]
+        public void PeekDoesNotAdvancesTheStreamPosition()
+        {
+            Stream<int> stream = new Stream<int>(new int[3] { 1, 2, 3 });
+            Assert.AreEqual(0, stream.Position);
+            stream.Peek();
+            Assert.AreEqual(0, stream.Position);
+        }
+
+        [TestMethod]
+        public void NextReturnsTheStreamElementsInOrder()
+        {
+            Stream<int> stream = new Stream<int>(new int[3] { 1, 2, 3 });
+            Assert.AreEqual(1, stream.Next());
+            Assert.AreEqual(2, stream.Next());
+            Assert.AreEqual(3, stream.Next());
+        }
+
+        [TestMethod]
+        public void UpToReturnsTheElementsFromTheCurrentPositionToTheFirstOccurrence()
+        {
+            Stream<int> stream = new Stream<int>(Enumerable.Range(1, 10));
+            IEnumerable<int> expected = new int[2] { 2, 3 };
+            stream.Next(); // Discard first
+            Assert.IsTrue(expected.SequenceEqual(stream.UpTo(4)));
+        }
+
+        [TestMethod]
+        public void UpToLimitIsNotInclusive()
+        {
+            Stream<int> stream = new Stream<int>(Enumerable.Range(1, 10));
+            stream.UpTo(4);
+            Assert.AreEqual(5, stream.Next());
+        }
+
+        [TestMethod]
+        public void UpToStopsAtTheEndIfOccurrenceNotFound()
+        {
+            Stream<int> stream = new Stream<int>(Enumerable.Range(1, 3));
+            IEnumerable<int> expected = new int[3] { 1, 2, 3 };
+            Assert.IsTrue(expected.SequenceEqual(stream.UpTo(11)));
+        }
+
+        [TestMethod]
+        public void UpToEndReturnsTheElementsFromCurrentPositionToTheEnd()
+        {
+            Stream<int> stream = new Stream<int>(Enumerable.Range(1, 5));
+            IEnumerable<int> expected = new int[3] { 3, 4, 5 };
+            stream.Next(); // Discard first
+            stream.Next(); // Discard second
+            Assert.IsTrue(expected.SequenceEqual(stream.UpToEnd()));
+        }
+
+        [TestMethod]
+        public void UpToAlsoAcceptsFunctionsAsParameter()
+        {
+            Stream<int> stream = new Stream<int>(Enumerable.Range(1, 5));
+            Func<int, bool> even = (n) => n % 2 == 0;
+            Assert.IsTrue(stream.UpTo(even).SequenceEqual(new int[1] { 1 }));
+            Assert.IsTrue(stream.UpTo(even).SequenceEqual(new int[1] { 3 }));
+            Assert.IsTrue(stream.UpTo(even).SequenceEqual(new int[1] { 5 }));
+            Assert.IsTrue(stream.UpTo(even).SequenceEqual(new int[0]));
+        }
+
+        [TestMethod]
+        public void UpToAcceptsANullParameter()
+        {
+            Stream<string> stream = new Stream<string>(new string[3] { "a", null, "b" });
+            Assert.IsTrue(stream.UpTo((string)null).SequenceEqual(new string[1] { "a" }));
+        }
+
+        [TestMethod]
+        public void NextAcceptsAnIntegerArgAndReturnsAsManyElementsAsTheArgSays()
+        {
+            Stream<int> stream = new Stream<int>(Enumerable.Range(1, 10));
+            Assert.IsTrue(stream.Next(3).SequenceEqual(new int[3] { 1, 2, 3 }));
+            Assert.IsTrue(stream.Next(2).SequenceEqual(new int[2] { 4, 5 }));
+        }
+
+        [TestMethod]
+        public void IfNextReachesTheEndItStopsRightThereRegardlessOfTheCountParameter()
+        {
+            Stream<int> stream = new Stream<int>(Enumerable.Range(1, 10));
+            stream.Next(5); // Discard the first half
+            Assert.IsTrue(stream.Next(10).SequenceEqual(new int[5] { 6, 7, 8, 9, 10 }));
+        }
+    }
+}
