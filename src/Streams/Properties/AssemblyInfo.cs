8fe512ce8b8a496c42cd7c04810f9c0def831757
RichoM <richi.moran@gmail.com>
2016-07-28 18:00:35 -0300

    Updated website.

--- src/Streams/Properties/AssemblyInfo.cs
+++ src/Streams/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RichoM.Streams")]
-[assembly: AssemblyDescription("Smalltalk-like streams in C#. Website: https://github.com/RichoM/Streams")]
+[assembly: AssemblyDescription("Smalltalk-like streams in C#. Website: https://github.com/RichoM/dotnet.Streams")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ricardo Moran")]
 [assembly: AssemblyProduct("RichoM.Streams")]
